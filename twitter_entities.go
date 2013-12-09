36398e97bf1c87a29ef99605e1a76848199e9e0c
Alexey Kachayev <kachayev@gmail.com>
2013-12-09 16:53:58 +0200

    Update twitter_entities.go

--- twitter_entities.go
+++ twitter_entities.go
@@ -2,20 +2,22 @@ package anaconda
 
 type TwitterEntities struct {
 	Hashtags []struct {
-		Indices []int
-		Text    string
-	}
+		Indices []int `json:"indices"`
+		Text    string `json:"text"`
+	} `json:"hashtags"`
+
 	Urls []struct {
-		Indices      []int
-		Url          string
-		Display_url  string
-		Expanded_url string
-	}
+		Indices      []int `json:"indices"`
+		Url          string `json:"url"`
+		Display_url  string `json:"display_url"`
+		Expanded_url string `json:"expanded_url"`
+	} `json:"urls"`
+
 	User_mentions []struct {
-		Name        string
-		Indices     []int
-		Screen_name string
-		Id          int64
-		Id_str      string
-	}
+		Name        string `json:"name"`
+		Indices     []int `json:"indices"`
+		Screen_name string `json:"screen_name"`
+		Id          int64 `json:"id"`
+		Id_str      string `json:"id_str"`
+	} `json:"user_mentions"`
 }
