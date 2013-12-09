3cbff8628a3347c9ab69ffa98a9d61773681d264
Alexey Kachayev <kachayev@gmail.com>
2013-12-09 16:51:59 +0200

    Update tweet.go

--- tweet.go
+++ tweet.go
@@ -1,15 +1,15 @@
 package anaconda
 
 type Tweet struct {
-	Source        string
-	Id            int64
-	Retweeted     bool
-	Favorited     bool
-	User          TwitterUser
-	Truncated     bool
-	Text          string
-	Retweet_count int64
-	Id_str        string
-	Created_at    string
-	Entities      TwitterEntities
+	Source        string `json:"source"`
+	Id            int64 `json:"id"`
+	Retweeted     bool `json:"retweeted"`
+	Favorited     bool `json:"favorited"`
+	User          TwitterUser `json:"user"`
+	Truncated     bool `json:"truncated"`
+	Text          string `json:"text"`
+	Retweet_count int64 `json:"retweet_count"`
+	Id_str        string `json:"id_str"`
+	Created_at    string `json:"created_at"`
+	Entities      TwitterEntities `json:"entities"`
 }
