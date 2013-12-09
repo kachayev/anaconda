cd441acbcd271722abe276b786243a7b8d3d9390
Alexey Kachayev <kachayev@gmail.com>
2013-12-09 16:53:18 +0200

    Update twitter_user.go

--- twitter_user.go
+++ twitter_user.go
@@ -1,41 +1,41 @@
 package anaconda
 
 type TwitterUser struct {
-	Statuses_count                     *int64
-	Contributors_enabled               *bool
-	Friends_count                      *int64
-	Geo_enabled                        *bool
-	Description                        *string
-	Profile_sidebar_border_color       *string
-	Screen_name                        *string
-	Listed_count                       *int64
-	Followers_count                    *int64
-	Location                           *string
-	Profile_background_image_url       *string
-	Name                               *string
-	Default_profile_image              *bool
-	Profile_image_url_https            *string
-	Notifications                      *bool
-	Protected                          *bool
-	Id_str                             *string
-	Profile_background_color           *string
-	Created_at                         *string
-	Default_profile                    *bool
-	Url                                *string
-	Id                                 *int64
-	Verified                           *bool
-	Profile_link_color                 *string
-	Profile_image_url                  *string
-	Profile_use_background_image       *bool
-	Favourites_count                   *int64
-	Profile_background_image_url_https *string
-	Profile_sidebar_fill_color         *string
-	Is_translator                      *bool
-	Follow_request_sent                *bool
-	Following                          *bool
-	Profile_background_tile            *bool
-	Show_all_inline_media              *bool
-	Profile_text_color                 *string
-	Lang                               *string
-	Entities                           *TwitterEntities
+	Statuses_count                     *int64 `json:"statuses_count"`
+	Contributors_enabled               *bool `json:"contributors_enabled"`
+	Friends_count                      *int64 `json:"friends_count"`
+	Geo_enabled                        *bool `json:"geo_enabled"`
+	Description                        *string `json:"description"`
+	Profile_sidebar_border_color       *string `json:"profile_sidebar_border_color"`
+	Screen_name                        *string `json:"screen_name"`
+	Listed_count                       *int64 `json:"listed_count"`
+	Followers_count                    *int64 `json:"followers_count"`
+	Location                           *string `json:"location"`
+	Profile_background_image_url       *string `json:"profile_background_image_url"`
+	Name                               *string `json:"name"`
+	Default_profile_image              *bool `json:"default_profile_image"`
+	Profile_image_url_https            *string `json:"profile_image_url_https"`
+	Notifications                      *bool `json:"notifications"`
+	Protected                          *bool `json:"protected"`
+	Id_str                             *string `json:"id_str"`
+	Profile_background_color           *string `json:"profile_background_color"`
+	Created_at                         *string `json:"created_at"`
+	Default_profile                    *bool `json:"default_profile"`
+	Url                                *string `json:"url"`
+	Id                                 *int64 `json:"id"`
+	Verified                           *bool `json:"verified"`
+	Profile_link_color                 *string `json:"profile_link_color"`
+	Profile_image_url                  *string `json:"profile_image_url"`
+	Profile_use_background_image       *bool `json:"profile_use_background_image"`
+	Favourites_count                   *int64 `json:"favourites_count"`
+	Profile_background_image_url_https *string `json:"profile_background_image_url_https"`
+	Profile_sidebar_fill_color         *string `json:"profile_sidebar_fill_color"`
+	Is_translator                      *bool `json:"is_translator"`
+	Follow_request_sent                *bool `json:"follow_request_sent"`
+	Following                          *bool `json:"following"`
+	Profile_background_tile            *bool `json:"profile_background_tile"`
+	Show_all_inline_media              *bool `json:"show_all_inline_media"`
+	Profile_text_color                 *string `json:"profile_text_color"`
+	Lang                               *string `json:"lang"`
+	Entities                           *ITwitterEntities `json:"entities"`
 }
